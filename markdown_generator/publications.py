0ebe97148579cca868cb4de2e4f8faac054e6f5e
Davidvandijcke <david.van.dijcke@gmail.com>
2025-01-25 11:52:28 -0500

    fixed rendering errors of symbols in pubs

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -39,8 +39,8 @@ os.chdir("/Users/davidvandijcke/Dropbox (University of Michigan)/website/markdow
 # In[3]:
 
 #publications = pd.read_csv("/home/antonvocalis/Dropbox (University of Michigan)/website/markdown_generator/publications2.csv", sep="\t", header=0, encoding = "utf-8")
-publications = pd.read_csv("publications2.csv", sep=",", header=0, encoding="latin1")
-wps =  pd.read_csv("wps.csv", sep=",", header=0, encoding = "latin1")
+publications = pd.read_csv("publications2.csv", header=0, encoding="utf-8")
+wps =  pd.read_csv("wps.csv", sep=",", header=0, encoding="utf-8")
 
 
 # ## Escape special characters
